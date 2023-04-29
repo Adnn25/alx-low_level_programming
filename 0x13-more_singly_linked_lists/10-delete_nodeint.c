09037b3f4d965bb96e4ce48d1babcdf4fb9d2749
Adnane Ameziane <adnane.ameziane@edu.uiz.ac.ma>
2023-04-29 04:50:33 -0700

    task

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,39 +1,37 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
- * @index: index of the node to delete
+ * delete_nodeint_at_index - deletes the node at index
+ * @head: double pointer
+ * @index: index of node
  *
- * Return: 1 (Success), or -1 (Fail)
+ * Return: pointer to the index node
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current = *head;
-	listint_t *previous = *head;
-	int pos = idx + 1;
+	unsigned int i;
+	listint_t *temp, *next;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
-
-	if (pos == 1)
+	if (index == 0)
 	{
-		*head = current->next;
-		free(current);
-		current = NULL;
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+		return (1);
 	}
-	else
+	temp = *head;
+	for (i = 0; i < index - 1; i++)
 	{
-		while (pos != 1)
-		{
-			previous = current;
-			current = current->next;
-			pos--;
-		}
-		previous->next = current->next;
-		free(current);
-		current = NULL;
+		if (temp->next == NULL)
+			return (-1);
+		temp = temp->next;
 	}
-
+	next = temp->next;
+	temp->next = next->next;
+	free(next);
 	return (1);
+
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,31 +1,42 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
+ * insert_nodeint_at_index - inserting a new node at agiven postion
+ * @head: double pointer
+ * @idx: index of the node
+ * @n: new node value
  *
- * Return: pointer to the new node, or NULL
+ * Return: the address of new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr = *head;
-	listint_t *ptr2 = malloc(sizeof(listint_t));
-	ptr2->n = n;
-	ptr2->next = NULL;
-	int pos = idx + 1;
+	unsigned int i;
+	listint_t *temp, *new;
 
-	pos--;
-	while (pos != 1)
+	if (head == NULL)
+		return (NULL);
+	if (idx != 0)
 	{
-		ptr = ptr->next;
-		pos--;
+		temp = *head;
+		for (i = 0; i < idx - 1 && temp != NULL; i++)
+		{
+			temp = temp->next;
+		}
+		if (temp == NULL)
+			return (NULL);
 	}
-
-	ptr2->next = ptr->next;
-	ptr->link = ptr2;
-
-	return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
 }
